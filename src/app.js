892d7a441a287c4f0d8c0e10b5e3e12eb88fff49
drrs <David.rubies@gmail.com>
2022-09-10 00:58:05 +0000

    Added DOM Dynamic Response Functionality

--- src/app.js
+++ src/app.js
@@ -7,5 +7,32 @@ import "./assets/img/4geeks.ico";
 
 window.onload = function() {
   //write your code here
-  console.log("Hello Rigo from the console!");
+  document.getElementById("excuse").innerHTML = excuseGenerator();
 };
+
+//returns a string composed of four random outputs of each array
+function excuseGenerator() {
+  let who = ["The dog", "My grandma", "His turtle", "My bird"];
+  let action = ["ate", "peed", "crushed", "broke"];
+  let what = ["my homework", "the keys", "the car"];
+  let when = [
+    "before the class",
+    "right on time",
+    "when I finished",
+    "during my lunch",
+    "while I was praying"
+  ];
+
+  function outputStringPerArray(inputArray) {
+    let outputString =
+      inputArray[Math.floor(Math.random() * inputArray.length)] + " ";
+    return outputString;
+  }
+
+  return (
+    outputStringPerArray(who) +
+    outputStringPerArray(action) +
+    outputStringPerArray(what) +
+    outputStringPerArray(when)
+  );
+}
